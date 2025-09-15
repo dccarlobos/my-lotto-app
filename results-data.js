ee6cff3f803804220e20e127cb2c7af4816895ac
dccarlobos <dccarlobos@gmail.com>
2025-09-15 18:05:45 +0800

    Add files via upload

---
+++ results-data.js
@@ -0,0 +1,80 @@
+// results-data.js
+const resultsData = {
+  "2025-09-12": [
+    { name: "6/58 Lotto", combination: "No results" },
+    { name: "6/55 Grand Lotto", combination: "No results" },
+    { name: "6/49 Lotto", combination: "No results" },
+    { name: "6/45 Mega Lotto", combination: "No results" },
+    { name: "6/42 Lotto", combination: "No results" },
+    { name: "6D Lotto", combination: "No results" },
+    { name: "4D Lotto", combination: "No results" },
+    {
+      name: "3D Lotto (Swertres)",
+      draws: [
+        { time: "2PM", numbers: "No results" },
+        { time: "5PM", numbers: "No results" },
+        { time: "9PM", numbers: "No results" }
+      ]
+    },
+    {
+      name: "2D Lotto (EZ2)",
+      draws: [
+        { time: "2PM", numbers: "No results" },
+        { time: "5PM", numbers: "No results" },
+        { time: "9PM", numbers: "No results" }
+      ]
+    }
+  ],
+
+  "2025-09-13": [
+    { name: "6/58 Lotto", combination: "No results" },
+    { name: "6/55 Grand Lotto", combination: "39 - 33 - 30 - 53 - 01 - 23" },
+    { name: "6/49 Lotto", combination: "No results" },
+    { name: "6/45 Mega Lotto", combination: "No results" },
+    { name: "6/42 Lotto", combination: "06 - 07 - 27 - 17 - 23 - 33" },
+    { name: "6D Lotto", combination: "8 - 5 - 9 - 0 - 1 - 0" },
+    { name: "4D Lotto", combination: "4 - 7 - 9 - 4" },
+    {
+      name: "3D Lotto (Swertres)",
+      draws: [
+        { time: "2PM", numbers: "5 - 1 - 8" },
+        { time: "5PM", numbers: "0 - 1 - 4" },
+        { time: "9PM", numbers: "3 - 3 - 0" }
+      ]
+    },
+    {
+      name: "2D Lotto (EZ2)",
+      draws: [
+        { time: "2PM", numbers: "8 - 20" },
+        { time: "5PM", numbers: "16 - 16" },
+        { time: "9PM", numbers: "14 - 2" }
+      ]
+    }
+  ],
+
+  "2025-09-14": [
+    { name: "6/58 Lotto", combination: "No results" },
+    { name: "6/55 Grand Lotto", combination: "No results" },
+    { name: "6/49 Lotto", combination: "No results" },
+    { name: "6/45 Mega Lotto", combination: "No results" },
+    { name: "6/42 Lotto", combination: "No results" },
+    { name: "6D Lotto", combination: "No results" },
+    { name: "4D Lotto", combination: "No results" },
+    {
+      name: "3D Lotto (Swertres)",
+      draws: [
+        { time: "2PM", numbers: "No results" },
+        { time: "5PM", numbers: "No results" },
+        { time: "9PM", numbers: "No results" }
+      ]
+    },
+    {
+      name: "2D Lotto (EZ2)",
+      draws: [
+        { time: "2PM", numbers: "No results" },
+        { time: "5PM", numbers: "No results" },
+        { time: "9PM", numbers: "No results" }
+      ]
+    }
+  ]
+};
